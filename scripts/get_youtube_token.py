984489cf2398581d6b775399a642b80959dd20fb
Yusufmmm <98128336+Yusufmmm@users.noreply.github.com>
2026-09-24 21:29:54 +0200

    auth: keep token request limited to upload

--- scripts/get_youtube_token.py
+++ scripts/get_youtube_token.py
@@ -3,14 +3,8 @@ import json
 from google_auth_oauthlib.flow import InstalledAppFlow
 
 data = json.loads(input("Paste OAuth client JSON: "))
-scopes = [
-    "https://www.googleapis.com/auth/youtube.upload",
-    "https://www.googleapis.com/auth/youtube.readonly",
-    "https://www.googleapis.com/auth/yt-analytics.readonly",
-]
-flow = InstalledAppFlow.from_client_config(data, scopes)
+flow = InstalledAppFlow.from_client_config(data, ["https://www.googleapis.com/auth/youtube.upload"])
 credentials = flow.run_local_server(port=0)
 print("YOUTUBE_CLIENT_ID=", credentials.client_id)
 print("YOUTUBE_CLIENT_SECRET=", credentials.client_secret)
 print("YOUTUBE_REFRESH_TOKEN=", credentials.refresh_token)
-
