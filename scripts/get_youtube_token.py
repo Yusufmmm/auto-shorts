003786939d8117856970dafb2136253461dd2f34
Yusufmmm <98128336+Yusufmmm@users.noreply.github.com>
2026-09-24 21:25:49 +0200

    auth: request upload and analytics scopes

--- scripts/get_youtube_token.py
+++ scripts/get_youtube_token.py
@@ -3,7 +3,12 @@ import json
 from google_auth_oauthlib.flow import InstalledAppFlow
 
 data = json.loads(input("Paste OAuth client JSON: "))
-flow = InstalledAppFlow.from_client_config(data, ["https://www.googleapis.com/auth/youtube.upload"])
+scopes = [
+    "https://www.googleapis.com/auth/youtube.upload",
+    "https://www.googleapis.com/auth/youtube.readonly",
+    "https://www.googleapis.com/auth/yt-analytics.readonly",
+]
+flow = InstalledAppFlow.from_client_config(data, scopes)
 credentials = flow.run_local_server(port=0)
 print("YOUTUBE_CLIENT_ID=", credentials.client_id)
 print("YOUTUBE_CLIENT_SECRET=", credentials.client_secret)
